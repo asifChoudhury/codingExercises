c1a5202e261145e4d39257e9055d959e2362ac27
Md Asif Choudhury <bdl365@yahoo.com>
2018-04-02 21:35:32 -0400

    Recursive Fibonacci sequence

---
+++ fibonacci.js
@@ -0,0 +1,27 @@
+/*
+    Recursive Fibonacci sequence.
+
+    1, 1, 2, 3, 5, 8, 13, 21, 34, 55, …
+*/
+
+fibonacci = (num) => {
+        if(num === 0) {
+            return 0;
+        } else if(num === 1) {
+            return 1;
+        }
+
+        return fibonacci(num - 1) + fibonacci(num - 2);;
+    }
+
+    console.log(fibonacci(0));
+    console.log(fibonacci(1));
+    console.log(fibonacci(2));
+    console.log(fibonacci(3));
+    console.log(fibonacci(4));
+    console.log(fibonacci(5));
+    console.log(fibonacci(6));
+    console.log(fibonacci(7));
+    console.log(fibonacci(8));
+    console.log(fibonacci(9));
+    console.log(fibonacci(10));
