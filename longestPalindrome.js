84207e23494a4d7a8f35e467ecd83bf072214be5
Md Asif Choudhury <bdl365@yahoo.com>
2018-04-04 13:18:07 -0400

    another solution for longest palindrome added

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -36,3 +36,70 @@ longestPalindrome = (str) => {
 longestPalindrome('abccccdd');
 longestPalindrome('abcd');
 longestPalindrome('abbcdde');
+
+//==============================================================================================================
+
+// Another Solution without using set
+longestPalindrome = (str) => {
+    let longestPalindrome = '';
+    console.log(`Get longest palindrome for '${str}'`);
+    let uniqueLetters = getUniqueLetters(str);
+    console.log(`Unique Letters: ${uniqueLetters}`);
+
+    let noPairLetters = [];
+    let count = 0;
+    console.log(`Letter counts - `);
+    for(let i = 0; i < uniqueLetters.length; i++) {
+        count = getNumOccurences(str, uniqueLetters.charAt(i));
+        console.log(`${uniqueLetters.charAt(i)} : ${count}`);
+
+        if(count % 2 === 0) {
+            let temp = 1;
+            while(temp <= count) {
+                longestPalindrome = uniqueLetters.charAt(i) + longestPalindrome + uniqueLetters.charAt(i);
+                temp += 2;
+            }
+        } else {
+            noPairLetters += uniqueLetters.charAt(i);
+        }
+    }
+
+    // insert the first lonely char
+    if(longestPalindrome.length >= 3) {
+        let res = longestPalindrome.split('');
+        res.splice((longestPalindrome.length / 2), 0, noPairLetters.charAt(0));
+        longestPalindrome = res.join('');
+    } else {
+        longestPalindrome = 'No palindrome';
+    }
+
+    console.log(`Longest palindrome for '${str}' is: ${longestPalindrome}`);
+    console.log(``);
+}
+
+getUniqueLetters = (str) => {
+    let uniqueLetters = '';
+
+    for(let i = 0; i < str.length; i++) {
+        if(uniqueLetters.indexOf(str.charAt(i)) === - 1) {
+            uniqueLetters += str.charAt(i);
+        }
+    }
+    return uniqueLetters;
+}
+
+getNumOccurences = (str, letterToMatch) => {
+    let count = 0;
+
+    for(let i = 0; i < str.length; i++) {
+        if(str.charAt(i) === letterToMatch) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+longestPalindrome('abccccdd');
+longestPalindrome('abcd');
+longestPalindrome('abbcdde');
